866f5cb825ed458c0b413871be8b1c960b6649cf
Lee Walton <lee.walton2014@outlook.com>
2021-07-21 04:10:13 +0100

    about and services section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,18 @@
+import React from "react";
+//import page
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+
+const AboutUs = () => {
+    //code here
+
+    return (
+        //stuff
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
+    );
+};
+
+export default AboutUs;
